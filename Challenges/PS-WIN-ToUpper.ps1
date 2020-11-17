52958ff71049407964984f0e79223b4ad008100b
tedelm <Ted.Elmenheim@itgarden.se>
2020-11-17 13:43:38 +0100

    challenge

--- Challenges/PS-WIN-ToUpper.ps1
+++ Challenges/PS-WIN-ToUpper.ps1
@@ -6,9 +6,7 @@ Make every first letter in word CAP-size
 
 $lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium suscipit beatae nemo quidem quas, ducimus dolorem deserunt in eius, minus iure dolorum quasi quis expedita? Amet nam deserunt distinctio eligendi?"
 
-$words = $lorem.split(" ")
-
-foreach ($word in $words) {
+foreach ($word in $lorem.split(" ")) {
     
     $loremOut += "$($word.substring(0,1).ToUpper()+$word.substring(1)) "
     
